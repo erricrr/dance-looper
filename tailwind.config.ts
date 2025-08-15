936fc314bb14e907c31441276ad4df32ceed3cb6
Erica Burgos <ericab79@gmail.com>
2025-08-15 16:03:12 +0000

    Use a more flow-like font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Lato', 'sans-serif'],
-        headline: ['Cinzel', 'serif'],
+        headline: ['"Cinzel Decorative"', 'serif'],
         code: ['monospace'],
       },
       colors: {
